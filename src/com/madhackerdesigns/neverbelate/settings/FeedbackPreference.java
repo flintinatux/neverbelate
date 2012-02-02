46107e3704d1f4fb1cdc7fb4464d3009adf6f48b
Scott McCormack <mail@madhackerdesigns.com>
2012-02-01 19:25:25 -0500

    Added case # to feedback subject

--- src/com/madhackerdesigns/neverbelate/settings/FeedbackPreference.java
+++ src/com/madhackerdesigns/neverbelate/settings/FeedbackPreference.java
@@ -51,7 +51,7 @@ public class FeedbackPreference extends Preference {
 		Intent i = new Intent(Intent.ACTION_SEND);
 		i.setType("text/plain");
 		i.putExtra(Intent.EXTRA_EMAIL, new String[] { res.getString(R.string.feedback_address) });
-		String subject = res.getString(R.string.feedback_subject) + " - " + (new Date().getTime());
+		String subject = res.getString(R.string.feedback_subject) + (new Date().getTime());
 		i.putExtra(Intent.EXTRA_SUBJECT, subject);
 		mContext.startActivity(Intent.createChooser(i, res.getString(R.string.feedback_menu_title)));
 	}
