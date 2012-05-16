6597f0c749d296d370c06ea79e5a0bff6e578702
Scott McCormack <mail@madhackerdesigns.com>
2012-05-15 23:22:06 -0400

    Simplified the Registration class to only hold data in a SharedPrefs file

--- src/com/madhackerdesigns/neverbelate/ui/Registration.java
+++ src/com/madhackerdesigns/neverbelate/ui/Registration.java
@@ -5,36 +5,16 @@ import android.content.SharedPreferences;
 
 public class Registration {
 
-	// Private member fields for Registration data
-	private String betaCode;
-	private long birthdate;
-	private String email; 		// TODO: Create a way for beta version to download email per beta code.
-	private int gender;
-	private String firstName;
-	private String lastName;
-	private String promoCode;
-	private boolean registered;
-	private int zipCode;
-	
 	// SharedPreference keys for registration data
-	private static final String BETA_CODE = "beta_code";
-	private static final String BIRTHDATE = "birthdate";
-	private static final String GENDER = "gender";
+	private static final String EMAIL = "email";
 	private static final String FIRST_NAME = "first_name";
 	private static final String LAST_NAME = "last_name";
-	private static final String PROMO_CODE = "promo_code";
 	private static final String REGISTERED = "registered";
 	private static final String ZIP_CODE = "zip_code";
-	
-	// Static constants for gender of registered user
-	protected static final int GENDER_MALE = 0;
-	protected static final int GENDER_FEMALE = 1;
-	protected static final int GENDER_UNKNOWN = 2;
 	
 	// SharedPreferences stuff
 	private static final String REGISTRATION_DATA = "registration_data";
 	private SharedPreferences mSettings;
-	private SharedPreferences.Editor mEditor;
 	
 	
 	/**
@@ -42,153 +22,78 @@ public class Registration {
 	 * 
 	 */
 	public Registration(Context applicationContext) {
-		super();	// TODO: Is this necessary?
-		
 		// Load the Registration values and editor 
 		mSettings = applicationContext.getSharedPreferences(REGISTRATION_DATA, Context.MODE_PRIVATE);
-		mEditor = mSettings.edit();
-		
-		// Load Registration data if registered
-		if ( mSettings.contains(REGISTERED) ) {
-			registered = mSettings.getBoolean(REGISTERED, false);
-			if (registered) {
-				loadRegistration();
-			}
-		}
-	}
-
-	private void loadRegistration() {
-		betaCode = mSettings.getString(BETA_CODE, null);
-		birthdate = mSettings.getLong(BIRTHDATE, 0);
-		gender = mSettings.getInt(GENDER, GENDER_UNKNOWN);
-		firstName = mSettings.getString(FIRST_NAME, null);
-		lastName = mSettings.getString(LAST_NAME, null);
-		promoCode = mSettings.getString(PROMO_CODE, null);
-		zipCode = mSettings.getInt(ZIP_CODE, 0);
-	}
-	
-	protected void commit() {
-		mEditor.putString(BETA_CODE, betaCode);
-		mEditor.putLong(BIRTHDATE, birthdate);
-		mEditor.putInt(GENDER, gender);
-		mEditor.putString(FIRST_NAME, firstName);
-		mEditor.putString(LAST_NAME, lastName);
-		mEditor.putString(PROMO_CODE, promoCode);
-		mEditor.putBoolean(REGISTERED, registered);
-		mEditor.putInt(ZIP_CODE, zipCode);
-		mEditor.commit();
-	}
-
-	/**
-	 * @return the betaCode
-	 */
-	protected String getBetaCode() {
-		return betaCode;
-	}
-
-	/**
-	 * @param betaCode the betaCode to set
-	 */
-	protected void setBetaCode(String betaCode) {
-		this.betaCode = betaCode;
-	}
-
-	/**
-	 * @return the birthdate
-	 */
-	protected long getBirthdate() {
-		return birthdate;
-	}
-
-	/**
-	 * @param birthdate the birthdate to set
-	 */
-	protected void setBirthdate(long birthdate) {
-		this.birthdate = birthdate;
-	}
-
-	/**
-	 * @param email the email to set
-	 */
-	public void setEmail(String email) {
-		this.email = email;
 	}
 
 	/**
 	 * @return the email
 	 */
-	public String getEmail() {
-		return email;
+	protected String getEmail() {
+		return mSettings.getString(EMAIL, "");
 	}
-
+	
 	/**
-	 * @return the gender
+	 * @param email the email to set
 	 */
-	protected int getGender() {
-		return gender;
-	}
-
-	/**
-	 * @param gender the gender to set
-	 */
-	protected void setGender(int gender) {
-		this.gender = gender;
+	protected void setEmail(String email) {
+		mSettings.edit().putString(EMAIL, email);
 	}
 
 	/**
 	 * @return the firstName
 	 */
 	protected String getFirstName() {
-		return firstName;
+		return mSettings.getString(FIRST_NAME, "");
 	}
 
 	/**
 	 * @param firstName the firstName to set
 	 */
 	protected void setFirstName(String firstName) {
-		this.firstName = firstName;
+		mSettings.edit().putString(FIRST_NAME, firstName);
 	}
 
 	/**
 	 * @return the lastName
 	 */
 	protected String getLastName() {
-		return lastName;
+		return mSettings.getString(LAST_NAME, "");
 	}
 
 	/**
 	 * @param lastName the lastName to set
 	 */
 	protected void setLastName(String lastName) {
-		this.lastName = lastName;
+		mSettings.edit().putString(LAST_NAME, lastName);
 	}
 
 	/**
-	 * @return the promoCode
+	 * @return true if registration is completed
 	 */
-	protected String getPromoCode() {
-		return promoCode;
+	protected boolean isRegistered() {
+		return mSettings.getBoolean(REGISTERED, false);
 	}
 
 	/**
-	 * @param promoCode the promoCode to set
+	 * @param registered true if registration is completed
 	 */
-	protected void setPromoCode(String promoCode) {
-		this.promoCode = promoCode;
+	protected void setRegistered(boolean registered) {
+		mSettings.edit().putBoolean(REGISTERED, registered);
 	}
 
 	/**
 	 * @return the zipCode
 	 */
-	protected int getZipCode() {
-		return zipCode;
+	protected String getZipCode() {
+		return mSettings.getString(ZIP_CODE, "");
 	}
 
 	/**
 	 * @param zipCode the zipCode to set
 	 */
-	protected void setZipCode(int zipCode) {
-		this.zipCode = zipCode;
+	protected void setZipCode(String zipCode) {
+		mSettings.edit().putString(ZIP_CODE, zipCode);
 	}
 	
 }
